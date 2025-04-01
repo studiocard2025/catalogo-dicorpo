651058aa0e4493f51b00a382c611bd3142c0af8d
studiocard2025 <studiocard2025@gmail.com>
2025-04-01 02:07:51 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Catálogo DiCorpo</h1>
+      <p>Protótipo online com sucesso!</p>
+    </div>
+  );
+}
