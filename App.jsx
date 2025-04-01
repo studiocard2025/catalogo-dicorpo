778ee869bdeec4f6537a761076958f9a465c5edb
studiocard2025 <studiocard2025@gmail.com>
2025-04-01 02:04:29 -0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Catálogo DiCorpo</h1>
-      <p>Protótipo inicial no ar com sucesso!</p>
-    </div>
-  );
-}
